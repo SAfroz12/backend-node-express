f2935cbb28591f0eb8bb32f715ee50f4445acd37
Afroz Shaik <affrozali9342@gmail.com>
2026-07-09 08:19:07 +0530

    learned Helmet Middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const mongoose=require("mongoose");
+const helmet=require("helmet")
 const app=express();
+app.use(helmet())
 const studentRoutes=require("./routes/StudentsRoutes")
 const userRoutes=require("./routes/usersRoutes")
 app.use(express.json())
